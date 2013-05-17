7a169f3a2d610561829ae12e4fdc9f8c75e08dd9
Akshit Agarwal <akshit.jiit@gmail.com>
2013-05-18 03:27:03 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,44 +3,80 @@ from config import *
 from pickle import *
 from thread import *
 
-# Variable for checking if Client2 has connected 
-send_conn = None
+""" Server Class Handling both Clients by receiving data
+    from Client 1 and then sending data to Client 2.
 
-# Handling Client2 Connection and sending data
-def handleClient():
-    global send_conn
-    send_conn, addr = sendsock.accept()
-            
+    Have functionality of Threading for Client 2 so that
+    it can connect anytime with server
+"""
+class Server(object):
+    def __init__(self):
+        self.DATA = ""
+        self.send_connection = None
+
+    # Handling Client1 Connection and receiving data
+    def recvClient(self):
+        # Socket for reading arbitrary data from client 1
+        self.recv_socket = socket(AF_INET, SOCK_STREAM)
+        self.recv_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        host = (SERVER_HOST, SERVER_PORT)
+        self.recv_socket.bind(host)
+        self.recv_socket.listen(1)
+
+        # Accepting connection from Client 1
+        self.recv_connection, self.recvclient_address = self.recv_socket.accept()
+
+        temp = ""
+        # Receiving DATA from client1
+        while True:
+            data = self.recv_connection.recv(BUFSIZE)
+            if not data:
+                break
+            temp += data
+
+        self.DATA = temp
+        print 'Data Received from Client1 and Forwarded to Client2: \n' + \
+        str(loads(self.DATA))
+
+        # Closing the socket
+        self.recv_socket.close()
+
+    # Handling Client2 Connection and sending data
+    def sendClient(self):
+        # Socket for reading arbitrary data from client 1
+        self.send_socket = socket(AF_INET, SOCK_STREAM)
+        self.send_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        host = (SERVER_HOST, SERVER_PORT + 50)
+        self.send_socket.bind(host)
+        self.send_socket.listen(1)
+
+        # Accepting connection from Client 1
+        self.send_connection, self.sendclient_address = self.send_socket.accept()
+
+        # Closing the socket
+        self.send_socket.close()
+
+    # Waiting for Client1 to send data and Client2 to connect
+    def wait(self):
+        while self.DATA == "" or self.send_connection == None:
+            continue
+
+        # Sending data to Client2
+        self.send_connection.send(self.DATA)
+
+        # Closing both sockets
+        self.recv_socket.close()
+        self.send_socket.close()
+    
 if __name__ == '__main__':
-    # Socket for reading arbitrary data from client 1
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) 
-    host = (SERVER_HOST, SERVER_PORT)
-    sock.bind(host)
-    sock.listen(1)
+    # Making Server Class Object
+    server = Server()
     
-    # Socket for reading arbitrary data from client 1
-    sendsock = socket(AF_INET, SOCK_STREAM)
-    sendsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) 
-    host = (SERVER_HOST, SERVER_PORT + 50)
-    sendsock.bind(host)
-    sendsock.listen(1)
-    
-    # Starting thread for Client2 to Connect at anytime
-    start_new_thread(handleClient,())
-    
-    # Accepting connection from Client 1
-    rec_conn, addr = sock.accept()
-    ARBDATA = rec_conn.recv(BUFSIZE)
-    print 'Data Received from Client1 and Forwarded to Client2: \n' + \
-           str(loads(ARBDATA))
+    # Starting thread for Handling Client1
+    start_new_thread(server.sendClient,())
 
-    # Waiting for Client2 to Connect with Server
-    while send_conn == None: 
-        continue
+    # Handling Client1
+    server.recvClient()
     
-    # Sending data to Client2
-    send_conn.sendall(ARBDATA)
-    
-    # Closing the socket
-    sock.close()
+    # Waiting for Client1 to send data
+    server.wait()
